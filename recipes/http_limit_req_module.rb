346d482f3faf0ce1342752b4c0c7ad5e7205b1c7
Andres Rojas <andres@conpat.io>
2017-12-08 16:04:15 -0800

    Revert "Add http_limit_req_module support"

--- recipes/http_limit_req_module.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: http_limit_req_module
-#
-
-node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--with-http_limit_req_module"]
